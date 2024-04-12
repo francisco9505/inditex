0029c85fbd85b3961e224c93c75904e32af354ea
francisco9505 <franciscocastillo9505@gmail.com>
2024-04-12 11:29:25 +0200

    Add filter control

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,17 @@
-import "./Post.css"
+import "./Post.css";
 
-export const Post =(props)=>{
-    console.log(props);
-    return <>
-    <div className="post">
+export const Post = (props) => {
+  return (
+    <>
+      <div className="post">
         <div className="post__image-container">
-            <img className="post__image" src={props['im:image'][2].label}  />
+          <img className="post__image" src={props["im:image"][2].label} />
         </div>
         <div className="post__details">
-            <span className="post__name">{props['im:name'].label}</span>
-            <span className="post__author">{props['im:artist'].label}</span>
+          <span className="post__name">{props["im:name"].label}</span>
+          <span className="post__author">{props["im:artist"].label}</span>
         </div>
-    </div>
+      </div>
     </>
-}
+  );
+};
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,19 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Home.css";
 import { Post } from "../components/Post.jsx";
-import {mockData} from '../mockData.js';
+import { mockData } from "../mockData.js";
 
 const Home = () => {
-  const [podcastTotal, setPodcastTotal] = useState(0);
   const [postList, setPostList] = useState(mockData);
+  const [filteredPostList, setFilteredPostList] = useState(mockData);
+  const [filterValue, setFilterValue] = useState("");
+
+  useEffect(() => {
+    if (!filterValue) {
+      setFilteredPostList(postList);
+      return;
+    }
+    const filteredList = postList.filter((post) => {
+      const filter = filterValue.trim().toLowerCase();
+      const name = post["im:name"].label.toLowerCase();
+      const author = post["im:artist"].label.toLowerCase();
+      return name.includes(filter) || author.includes(filter);
+    });
+    setFilteredPostList(filteredList);
+  }, [postList, filterValue]);
+
   return (
     <div className="home">
       <div className="home__controls">
-        <span>{podcastTotal}</span>
-        <input type="text" />
+        <span className="home__filtered-list-length">
+          {filteredPostList.length}
+        </span>
+        <input
+          className="home__filter-input"
+          type="text"
+          placeholder="Filter podcast"
+          onChange={(e) => setFilterValue(e.target.value)}
+        />
       </div>
       <div className="home__podcast-list-container">
-        {postList.map((post) => (
+        {filteredPostList.map((post) => (
           <Post key={post.id.attributes["im:id"]} {...post} />
         ))}
       </div>

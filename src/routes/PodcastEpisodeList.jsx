ebc97abf3863664350db4cbc2d3e05799062525b
francisco9505 <franciscocastillo9505@gmail.com>
2024-04-16 19:21:45 +0200

    Fix timer for undefined episodes long

--- src/routes/PodcastEpisodeList.jsx
+++ src/routes/PodcastEpisodeList.jsx
@@ -1,44 +1,61 @@
 import * as PropTypes from "prop-types";
-import './PodcastEpisodeList.css';
-import {useNavigate, useParams} from "react-router-dom";
+import "./PodcastEpisodeList.css";
+import { useNavigate, useParams } from "react-router-dom";
 
 function millisToMinutesAndSeconds(millis) {
-  const minutes = Math.floor(millis / 60000);
-  const seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  if (!!millis) {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  }
+  return "00:00";
 }
 
-PodcastEpisodeList.propTypes = {list: PropTypes.array };
+PodcastEpisodeList.propTypes = { list: PropTypes.array };
 
-export function PodcastEpisodeList({list}) {
+export function PodcastEpisodeList({ list }) {
   const navigate = useNavigate();
-  const {podcastId} = useParams();
+  const { podcastId } = useParams();
 
   function openTrack(trackId) {
     navigate(`/podcast/${podcastId}/episode/${trackId}`);
   }
 
-  return (<div className="podcast__episode-list">
-    <div className="podcast__episode-list-count podcast__card">Episodes: {list.length}</div>
-    <div className="podcast__card">
-      <table className="podcast__table">
-        <thead>
-        <tr>
-          <th>Title</th>
-          <th>Date</th>
-          <th>Duration</th>
-        </tr>
-        </thead>
-        <tbody>
-        {list.map(({trackId, trackName, releaseDate, trackTimeMillis}, index) => (
-          <tr className={index % 2 === 0 ? 'podcast__episode-row gray' : 'podcast__episode-row'} key={trackId} onClick={() => openTrack(trackId)}>
-            <td className="blue">{trackName}</td>
-            <td>{new Date(releaseDate).toLocaleDateString()}</td>
-            <td>{millisToMinutesAndSeconds(trackTimeMillis)}</td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
+  return (
+    <div className="podcast__episode-list">
+      <div className="podcast__episode-list-count podcast__card">
+        Episodes: {list.length}
+      </div>
+      <div className="podcast__card">
+        <table className="podcast__table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map(
+              ({ trackId, trackName, releaseDate, trackTimeMillis }, index) => (
+                <tr
+                  className={
+                    index % 2 === 0
+                      ? "podcast__episode-row gray"
+                      : "podcast__episode-row"
+                  }
+                  key={trackId}
+                  onClick={() => openTrack(trackId)}
+                >
+                  <td className="blue">{trackName}</td>
+                  <td>{new Date(releaseDate).toLocaleDateString()}</td>
+                  <td>{millisToMinutesAndSeconds(trackTimeMillis)}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>);
+  );
 }
